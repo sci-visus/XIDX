2a207afe0d498d8d843ee0ec28e7cdd4d0897747
spetruzza <steve.petruzza@gmail.com>
2018-05-31 15:56:36 -0600

    cleanup push

--- include/xidx/setup.py
+++ include/xidx/setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup, Extension
 from glob import glob
 
 XidxPy_module = Extension('_XidxPy', 
-                          sources=['xidx.i'],#,'../../examples/cpp/write.cpp'], 
+                          sources=['xidx.i'],
                           include_dirs=['../','./elements','/usr/include/libxml2/'], 
                           libraries=['xml2'],
                           swig_opts=["-c++"],
-                          extra_compile_args=['-std=c++11'], #,'-stdlib=libc++'],
+                          extra_compile_args=['-std=c++11'], 
                           language="c++") 
 
 setup (name = 'XidxPy',
