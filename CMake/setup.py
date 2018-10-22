f49855bb60712190ffc93a098b7bf0927d763658
spetruzza <steve.petruzza@gmail.com>
2018-10-22 17:47:46 -0600

    fix wheels generation

--- CMake/setup.py
+++ CMake/setup.py
@@ -12,66 +12,6 @@ WIN32=platform.system()=="Windows" or platform.system()=="win32"
 APPLE=platform.system()=="Darwin"
 BDIST_WHEEL="bdist_wheel" in sys.argv
 
-src_path=sys.argv[0]+"/../../"
-#print("args",sys.argv)
-
-# Xidx_module = Extension('_Xidx',
-#                       sources=[src_path+'/include/xidx/xidx.i'],
-#                       include_dirs=[src_path+'/include/xidx',src_path+'/include/xidx/elements'],#,'/usr/include/libxml2/'],
-#                       libraries=['xml2'],
-#                       swig_opts=["-c++"],
-#                       extra_compile_args=['-std=c++11'],
-#                       language="c++")
-#
-# setup (name = 'Xidx',
-#    version = '0.0',
-#    author      = "University of Utah",
-#    description = """Xidx Python module""",
-#   # ext_modules = [Xidx_module],
-#    py_modules = ["_Xidx"],
-#   )
-#
-
-# ////////////////////////////////////////////////////////////////////
-def findFilesInCurrentDirectory():
-
-    ret=[]
-
-    for dirpath, __dirnames__, filenames in os.walk("."):
-        for it in filenames:
-
-            filename= os.path.abspath(os.path.join(dirpath, it))
-
-            if filename.startswith(os.path.abspath('./dist')):
-                continue
-
-            if os.path.basename(filename).startswith(".git"):
-                continue
-
-            if "__pycache__" in filename:
-                continue
-
-            # for bdist_wheel I don't need to add files for compilation
-            if BDIST_WHEEL:
-
-                if filename.startswith(os.path.abspath('./lib')):
-                    continue
-
-                if filename.startswith(os.path.abspath('./include')):
-                    continue
-
-                if WIN32:
-
-                    if filename.startswith(os.path.abspath('./win32/python')):
-                        continue
-
-                    if filename.endswith(".pdb"):
-                        continue
-
-            ret.append(filename)
-
-    return ret
-
 # ////////////////////////////////////////////////////////////////////
 def cleanAll():
     shutil.rmtree('./build', ignore_errors=True)
@@ -83,14 +23,14 @@ def runSetupTools():
 
     setuptools.setup(
         name = "Xidx",
-        description = "Xidx extensible metadata",
+        description = "Xidx extensible IDX library",
         version=VERSION,
         url="https://github.com/sci-visus/XIDX",
         author="visus.org",
         author_email="support@visus.org",
         packages=["Xidx"],
         package_dir={"Xidx":'.'},
-        package_data={"Xidx": '_xidx.so, xidx.py'},
+        package_data={"Xidx": ['lib/_xidx.so','lib/xidx.py']},
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: BSD License",
